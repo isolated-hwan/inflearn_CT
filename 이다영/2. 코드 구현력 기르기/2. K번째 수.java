6b11ddd564f8576242ec9aad75c83a12d92e1e56
이다영 <yooung513@gmail.com>
2024-07-18 00:56:34 +0900

    2. K번째 수

--- 이다영/2. 코드 구현력 기르기/2. K번째 수.java
+++ 이다영/2. 코드 구현력 기르기/2. K번째 수.java
@@ -1,10 +1,33 @@
-package main.java.org.example.others;
+package main.java.org.example.inflearn.sec02;
 
-import java.sql.Array;
 import java.util.*;
+import java.io.*;
 
 // K번째 수
 public class Inflearn2_2 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st1.nextToken());
+            int s = Integer.parseInt(st1.nextToken());
+            int e = Integer.parseInt(st1.nextToken());
+            int k = Integer.parseInt(st1.nextToken());
+
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int index = 0;
+            List<Integer> num = new ArrayList<>();
+            for (int j = 1; j <= n; j++) {
+                index++;
+
+                if (s <= index && index <= e) {
+                    num.add(j);
+                }
+            }
+            Collections.sort(num);
+            System.out.println("#" + (i+1) + " " + num.get(k));
+        }
     }
 }
